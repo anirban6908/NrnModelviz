32b450a5c41f632608e2825f87a1fe1238df1891
Ani Nandi <anirban6908@gmail.com>
2019-07-09 17:27:56 -0700

    Bluepyopt simulation with DC step

--- model_viz_test.py
+++ model_viz_test.py
@@ -7,6 +7,7 @@ import os
 import bluepyopt as bpopt
 import bluepyopt.ephys as ephys
 from ateamopt.bpopt_evaluator import Bpopt_Evaluator
+from ateamopt.utils import utility
 
 
 pg.setConfigOption('background', 'w')
@@ -22,7 +23,7 @@ class NrnModelViz(QtGui.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.sim_components = os.path.join(os.getcwd(),'sim_components')
-        self.morph_path = glob.glob(self.sim_components+'*.swc')
+        self.morph_path = glob.glob(self.sim_components+'/*.swc')
         if len(self.morph_path) > 1:
             raise Exception
         else:
@@ -36,6 +37,9 @@ class NrnModelViz(QtGui.QMainWindow, Ui_MainWindow):
                               total_duration=2270):
         
         protocol_name = 'DC_stim'
+        
+        
+        
         soma_loc = ephys.locations.NrnSeclistCompLocation(
             name='soma',
             seclist_name='somatic',
@@ -51,34 +55,50 @@ class NrnModelViz(QtGui.QMainWindow, Ui_MainWindow):
         
         stimuli = []
         stimuli.append(ephys.stimuli.NrnSquarePulse(
-            step_amplitude=stim_amp,
-            step_delay=delay,
-            step_duration=duration,
+            step_amplitude=stim_amp, 
+            step_delay=float(delay),
+            step_duration=float(duration),
             location=soma_loc,
-            total_duration=total_duration))
+            total_duration=float(total_duration)))
         
         
-        stim_protocol = {protocol_name : ephys.protocols.SweepProtocol(
+        stim_protocol =  ephys.protocols.SweepProtocol(
                     protocol_name,
                     stimuli,
-                    recordings)}
-        return stim_protocol
+                    recordings)
+        return stim_protocol,protocol_name
     
     def run_bpopt_sim(self):
-        stim_amp = float(self.stimAmp.toPlainText()) # in pA
-        stim_protocol = self.prepare_stim_protocol(stim_amp)
+        stim_amp = float(self.stimAmp.value())*1e-3 # in nA
+        stim_protocol,protocol_name = self.prepare_stim_protocol(stim_amp)
+        param_path = os.path.join(self.bpopt_config,'parameters.json')
+        mech_path= os.path.join(self.bpopt_config,'mechanism.json')
+        release_param_path = os.path.join(self.bpopt_config,'release_param.json')
+        release_params = utility.load_json(release_param_path)
         
         morphology = ephys.morphologies.\
-            NrnFileMorphology(self.morph_path, do_replace_axon=True)
+            NrnFileMorphology(self.morph_path, stub_axon=True)
         sim = ephys.simulators.NrnSimulator()
-        eval_handler = Bpopt_Evaluator(
-                   morph_path, param_path,
-                   mech_path,timed_evaluation = False)
+        eval_handler = Bpopt_Evaluator(protocol_path=None,
+                           feature_path=None,
+                           morph_path=None, param_path=param_path,
+                           mech_path=mech_path)
+        mechanisms = eval_handler.define_mechanisms()
+        parameters = eval_handler.define_parameters()
+        
+        model_aa = ephys.models.CellModel('aa_model',
+                       morph=morphology, mechs=mechanisms, 
+                       params=parameters)
+            
+        responses = stim_protocol.run(model_aa,release_params,sim)
+        time_response = responses['{}.soma.v'.format(protocol_name)]['time']
+        voltage_response =responses['{}.soma.v'.format(protocol_name)]['voltage']
         
         self.simview.clear()
-        a=np.random.randn(10)
-        b=np.random.randn(10)
-        self.simview.plot(a,b,pen=pg.mkPen('k', width=2))
+#        a=np.random.randn(10)
+#        b=np.random.randn(10)
+        self.simview.plot(time_response,voltage_response,
+                          pen=pg.mkPen('k', width=2))
         
  
 if __name__ == "__main__":
